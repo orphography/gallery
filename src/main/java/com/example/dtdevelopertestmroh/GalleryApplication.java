3d3535d00fc226257038fc5b7ad8dd164b18414b
ASUTP <awsefeawsf@mail.ru>
2024-12-09 15:05:30 +0300

    Вынес создание верхней панели в отдельный класс TopPanel

--- src/main/java/com/example/dtdevelopertestmroh/GalleryApplication.java
+++ src/main/java/com/example/dtdevelopertestmroh/GalleryApplication.java
@@ -14,50 +14,16 @@ import java.io.IOException;
 
 public class GalleryApplication extends javafx.application.Application {
     private GalleryPanel galleryPanel;
-    private boolean isSortedByName = false;
-    private boolean isSortedByDate = false;
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Галерея изображений");
 
-        BorderPane borderPane = new BorderPane();
         galleryPanel = new GalleryPanel();
         galleryPanel.setPrefTileWidth(85);
         galleryPanel.setPrefTileHeight(85);
-        HBox topPanel = new HBox(2);
 
-        TextField searchField = new TextField();
-        searchField.setPromptText("Поиск изображений");
-        searchField.textProperty().addListener((ov, oldV, newV) -> {
-            galleryPanel.filterImages(newV);
-        });
-
-        Button loadButton = new Button(" Загрузить в галерею");
-        loadButton.setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.jpeg"));
-            File file = fileChooser.showOpenDialog(stage);
-            if (file != null) {
-                galleryPanel.saveImage(file);
-            }
-        });
-
-        Button sortByName = new Button(" Сортировать по имени ↑");
-        sortByName.setOnAction(event -> {
-            galleryPanel.sortByName(isSortedByName);
-            isSortedByName = !isSortedByName;
-            sortByName.setText(isSortedByName ? " Сортировать по имени ↓" : " Сортировать по имени ↑");
-        });
-
-        Button sortByDate = new Button(" Сортировать по дате ↑");
-        sortByDate.setOnAction(event -> {
-            galleryPanel.sortByDate(isSortedByDate);
-            isSortedByDate = !isSortedByDate;
-            sortByDate.setText(isSortedByDate ? " Сортировать по дате ↓" : " Сортировать по дате ↑");
-        });
-
-        topPanel.getChildren().addAll(searchField, loadButton, sortByName, sortByDate);
-        borderPane.setTop(topPanel);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(new TopPanel(stage, galleryPanel));
 
         galleryPanel.setOnImagesLoaded(event -> {
             borderPane.setCenter(galleryPanel);
---
+++ src/main/java/com/example/dtdevelopertestmroh/TopPanel.java
@@ -0,0 +1,46 @@
+package com.example.dtdevelopertestmroh;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class TopPanel extends HBox {
+    private boolean isSortedByName = false;
+    private boolean isSortedByDate = false;
+    public TopPanel(Stage stage, GalleryPanel galleryPanel) {
+
+        TextField searchField = new TextField();
+        searchField.setPromptText("Поиск изображений");
+        searchField.textProperty().addListener((ov, oldV, newV) -> galleryPanel.filterImages(newV));
+
+        Button loadButton = new Button(" Загрузить в галерею");
+        loadButton.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.jpeg"));
+            File file = fileChooser.showOpenDialog(new Stage());
+            if (file != null) {
+                galleryPanel.saveImage(file);
+            }
+        });
+
+        Button sortByName = new Button(" Сортировать по имени ↑");
+        sortByName.setOnAction(event -> {
+            galleryPanel.sortByName(isSortedByName);
+            isSortedByName = !isSortedByName;
+            sortByName.setText(isSortedByName ? " Сортировать по имени ↓" : " Сортировать по имени ↑");
+        });
+
+        Button sortByDate = new Button(" Сортировать по дате ↑");
+        sortByDate.setOnAction(event -> {
+            galleryPanel.sortByDate(isSortedByDate);
+            isSortedByDate = !isSortedByDate;
+            sortByDate.setText(isSortedByDate ? " Сортировать по дате ↓" : " Сортировать по дате ↑");
+        });
+
+        getChildren().addAll(searchField, loadButton, sortByName, sortByDate);
+    }
+}
